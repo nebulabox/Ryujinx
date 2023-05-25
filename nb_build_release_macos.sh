d419e39cdc182bbf4d807e61ee37d4208c03fa79
nebulabox <nebulabox@gmail.com>
2023-05-25 13:35:34 +0800

    no message

--- nb_build_release_macos.sh
+++ nb_build_release_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 # brew install dotnet-sdk
-VERISON="1.1.800"
+VERISON="1.1.819"
 ./distribution/macos/create_macos_build.sh ./ ./distribution/macos/temp ./distribution/macos/output ./distribution/macos/entitlements.xml $VERISON 0 "Release"
 
